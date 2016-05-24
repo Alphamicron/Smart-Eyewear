d0c0217ac1b87542bec6beb7d7a26ded37721b6d
Emil Shirima <emil.shirima@hotmail.com>
2016-05-24 18:43:42 -0400

    Finished the animation portion

--- iOS/Smart Eyewear/Smart Eyewear/BatteryLevelVC.swift
+++ iOS/Smart Eyewear/Smart Eyewear/BatteryLevelVC.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import PNChart
 
 class BatteryLevelVC: UIViewController
 {
-
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        drawCircleGraph()
+        
+//        drawTwo()
     }
 
     override func didReceiveMemoryWarning()
@@ -26,8 +28,35 @@ class BatteryLevelVC: UIViewController
     
     func drawCircleGraph()
     {
+        
+        let circleFrame: CGRect = CGRect(x: self.view.bounds.size.width/2, y: self.view.bounds.size.width/5, width: self.view.bounds.size.width/65, height: self.view.bounds.size.height/2)
+        let circleChart = PNCircleChart(frame: circleFrame, total: 100, current: 45, clockwise: true, shadow: true, shadowColor: UIColor.clearColor())
+
+        circleChart.displayCountingLabel = true
+        circleChart.lineWidth = 25
+        circleChart.backgroundColor = UIColor.clearColor()
+        circleChart.strokeColor = UIColor(red: 0.302, green: 0.769, blue: 0.478, alpha: 1.00)
+        circleChart.strokeChart()
+        
+        self.view.addSubview(circleChart)
     }
     
+    func drawTwo()
+    {
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: self.view.bounds.size.width/2,y: self.view.bounds.size.width/2), radius: CGFloat(100), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.CGPath
+        
+        //change the fill color
+        shapeLayer.fillColor = UIColor.clearColor().CGColor
+        //you can change the stroke color
+        shapeLayer.strokeColor = UIColor(red: 0.302, green: 0.769, blue: 0.478, alpha: 1.00).CGColor
+        //you can change the line width
+        shapeLayer.lineWidth = 10
+        
+        view.layer.addSublayer(shapeLayer)
+    }
 
     /*
     // MARK: - Navigation
