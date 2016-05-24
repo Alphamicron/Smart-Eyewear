a1163731f885fad75c934a30ee6fc0408365946c
Emil Shirima <emil.shirima@hotmail.com>
2016-05-24 12:04:59 -0400

    Finalized DevicesTVC but no bluetooth added yet

--- iOS/Smart Eyewear/Smart Eyewear/DevicesTVC.swift
+++ iOS/Smart Eyewear/Smart Eyewear/DevicesTVC.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class DeviceTVCell: UITableViewCell
 {
-    
+    @IBOutlet weak var deviceName: UILabel!
+    @IBOutlet weak var deviceState: UILabel!
+    @IBOutlet weak var deviceUUID: UILabel!
 }
 
 class DevicesTVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -34,23 +38,26 @@ class DevicesTVC: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! DeviceTVCell
+        
+        cell.deviceName.text = "Sample"
+        cell.deviceUUID.text = "1234567890326378899876556"
+        cell.deviceState.text = "Disconnected"
 
         // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
