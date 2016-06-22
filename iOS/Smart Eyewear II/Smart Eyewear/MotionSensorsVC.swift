71d2bebb571131a9fcee4bac2cc6e5e2f2c20192
Emil Shirima <alphamicron@Alphamicrons-Mac-mini.local>
2016-06-22 15:08:29 -0400

    Removed OthersVC and replaced it into MotionSensorsVC

--- iOS/Smart Eyewear II/Smart Eyewear/MotionSensorsVC.swift
+++ iOS/Smart Eyewear II/Smart Eyewear/MotionSensorsVC.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class MotionSensorsVC: UIViewController
 {
+    @IBOutlet weak var accelerometerBtn: UIButton!
+    @IBOutlet weak var gyroscopeBtn: UIButton!
+    @IBOutlet weak var magnetometerBtn: UIButton!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
+        // increase character spacing
+        accelerometerBtn.titleLabel?.kerning = 1.0
+        gyroscopeBtn.titleLabel?.kerning = (accelerometerBtn.titleLabel?.kerning)!
+        magnetometerBtn.titleLabel?.kerning = (accelerometerBtn.titleLabel?.kerning)!
     }
     
     override func viewWillAppear(animated: Bool)
