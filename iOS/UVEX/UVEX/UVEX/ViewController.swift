dee7d285ce4463da11d110b86b1b4ba5a98c6a58
Emil Shirima <alphamicron@Alphamicrons-Mac-mini.local>
2016-07-25 13:30:38 -0400

    Started the final UVEX project

---
+++ iOS/UVEX/UVEX/UVEX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UVEX
+//
+//  Created by Alphamicron on 7/25/16.
+//  Copyright © 2016 Alphamicron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
