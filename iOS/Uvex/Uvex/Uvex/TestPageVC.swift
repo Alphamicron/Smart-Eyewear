05bd7cc0f79711a5accd8f4e01b8b5358fbcae0f
Emil Shirima <alphamicron@Alphamicrons-Mac-mini.local>
2016-07-28 12:32:59 -0400

    Fixed the UIPageController navigation issue

--- iOS/Uvex/Uvex/Uvex/TestPageVC.swift
+++ iOS/Uvex/Uvex/Uvex/TestPageVC.swift
@@ -22,7 +22,7 @@ class TestPageVC: UIPageViewController
         let eyeWearVC = storyboard!.instantiateViewControllerWithIdentifier("eyeWear") as! EyeWearVC
         let fitnessVC = storyboard!.instantiateViewControllerWithIdentifier("fitness") as! FitnessVC
         let healthVC = storyboard!.instantiateViewControllerWithIdentifier("health") as! HealthVC
-        let environmentVC = storyboard!.instantiateViewControllerWithIdentifier("fitness") as! FitnessVC
+        let environmentVC = storyboard!.instantiateViewControllerWithIdentifier("environment") as! EnvironmentVC
         
         pages.append(eyeWearVC)
         pages.append(fitnessVC)
@@ -32,33 +32,21 @@ class TestPageVC: UIPageViewController
         setViewControllers([eyeWearVC], direction: .Forward, animated: true, completion: nil)
     }
     
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    func viewControllerAtIndex(index: Int)-> UIViewController
+    {
+        if self.pages.count == 0 || index >= self.pages.count
+        {
+            return UIViewController()
+        }
+        
+        return pages[index]
+    }
 }
-
-
-//func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-//    let currentIndex = pages.indexOf(viewController)!
-//    let previousIndex = abs((currentIndex - 1) % pages.count)
-//    return pages[previousIndex]
-//}
-//
-//func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-//    let currentIndex = pages.indexOf(viewController)!
-//    let nextIndex = abs((currentIndex + 1) % pages.count)
-//    return pages[nextIndex]
-//}
-//
-//func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
-//    return pages.count
-//}
-//
-//func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
-//    return 0
-//}
 
 extension TestPageVC: UIPageViewControllerDelegate
 {
@@ -79,16 +67,35 @@ extension TestPageVC: UIPageViewControllerDataSource
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController?
     {
-        let currentIndex: Int = pages.indexOf(viewController)!
-        let previousIndex:Int = abs((currentIndex - 1) % pages.count)
-        return pages[previousIndex]
+        var currentIndex: Int = pages.indexOf(viewController)!
+        
+        if currentIndex == 0 || currentIndex == NSNotFound
+        {
+            return nil
+        }
+        
+        currentIndex -= 1
+        return self.viewControllerAtIndex(currentIndex)
+        
     }
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController?
     {
-        let currentIndex: Int = pages.indexOf(viewController)!
-        let nextIndex:Int = abs((currentIndex + 1) % pages.count)
-        return pages[nextIndex]
+        var currentIndex: Int = pages.indexOf(viewController)!
+        
+        if currentIndex == NSNotFound
+        {
+            return nil
+        }
+        
+        currentIndex += 1
+        
+        if currentIndex == pages.count
+        {
+            return nil
+        }
+        
+        return self.viewControllerAtIndex(currentIndex)
     }
 }
 
