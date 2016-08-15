8613b545fa79d048b396fd8bf185379922586ebf
Emil Shirima <alphamicron@Alphamicrons-Mac-mini.local>
2016-08-15 13:54:29 -0400

    Added a border line to the textlabel and a navBar that matches the default app navBar

--- iOS/Uvex/Uvex/Uvex/ConnectionVC.swift
+++ iOS/Uvex/Uvex/Uvex/ConnectionVC.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ConnectionVC: UIViewController
 {
+    @IBOutlet weak var bluetoothImageView: UIImageView!
+    @IBOutlet weak var metawearStateLabel: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        setupTheUI()
     }
     
     override func viewWillAppear(animated: Bool)
@@ -32,5 +37,22 @@ class ConnectionVC: UIViewController
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
+    }
+    
+    func setupTheUI()
+    {
+        let layer: CALayer = self.metawearStateLabel.layer
+        let bottomBorder: CALayer = CALayer(layer: layer)
+        
+        bottomBorder.borderWidth = 3
+        bottomBorder.frame = CGRectMake(-bottomBorder.borderWidth, layer.frame.size.height-bottomBorder.borderWidth, layer.frame.size.width, bottomBorder.borderWidth)
+        bottomBorder.borderColor = UIColor(red: 0.282, green: 0.278, blue: 0.278, alpha: 1.00).CGColor
+        
+        layer.addSublayer(bottomBorder)
+    }
+    
+    @IBAction func connectionAction(sender: UIButton)
+    {
+        
     }
 }
