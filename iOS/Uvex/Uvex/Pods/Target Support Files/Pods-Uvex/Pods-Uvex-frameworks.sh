3e5034ba9876ac4ffe0881f10881c0384c3cf683
Emil Shirima <alphamicron@Alphamicrons-Mac-mini.local>
2016-08-19 11:50:29 -0400

    Designed the heart rate view successfully

--- iOS/Uvex/Uvex/Pods/Target Support Files/Pods-Uvex/Pods-Uvex-frameworks.sh
+++ iOS/Uvex/Uvex/Pods/Target Support Files/Pods-Uvex/Pods-Uvex-frameworks.sh
@@ -86,6 +86,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Charts/Charts.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/EPShapes/EPShapes.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FastCoding/FastCoding.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JSSAlertView/JSSAlertView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/PNChart/PNChart.framework"
@@ -94,6 +95,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Charts/Charts.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/EPShapes/EPShapes.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FastCoding/FastCoding.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JSSAlertView/JSSAlertView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/PNChart/PNChart.framework"
