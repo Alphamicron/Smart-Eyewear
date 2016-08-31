ecac1f053bedcec2eedbd7d2ebfdabd031bdf61b
Emil Shirima <emil.shirima@hotmail.com>
2016-08-30 21:46:37 -0400

    Moved the Service initalizing method into init

--- iOS/Uvex/Uvex/Uvex/Services.swift
+++ iOS/Uvex/Uvex/Uvex/Services.swift
@@ -62,40 +62,6 @@ class Services
         allServices[.Environment] = allEnvironmentServices
     }
     
-    //    private func addServices()
-    //    {
-    //        //        // Eyewear Services
-    //        //        let batteryLevel: Service = Service(serviceIcon: UIImage(named: "Battery")!, serviceName: "Battery Level")
-    //        //        let autoMode: Service = Service(serviceIcon: UIImage(named: "AutomaticBtn")!, serviceName: "Automatic")
-    //        //        let manualMode: Service = Service(serviceIcon: UIImage(named: "ManualBtn")!, serviceName: "Manual")
-    //        //        
-    //        //        // Fitness Services
-    //        //        let steps: Service = Service(serviceIcon: UIImage(named: "Steps")!, serviceName: "Steps")
-    //        //        let calories: Service = Service(serviceIcon: UIImage(named: "Fire")!, serviceName: "Calories")
-    //        //        let distance: Service = Service(serviceIcon: UIImage(named: "Map")!, serviceName: "Distance")
-    //        //        
-    //        //        // Health Services
-    //        //        let heartRate: Service = Service(serviceIcon: UIImage(named: "Heart-1")!, serviceName: "Heart Rate")
-    //        //        let oxygenLevel: Service = Service(serviceIcon: UIImage(named: "Lungs")!, serviceName: "SpO2")
-    //        //        let skinTemperature: Service = Service(serviceIcon: UIImage(named: "Thermostat")!, serviceName: "Skin Temperature")
-    //        //        
-    //        //        // Environment Services
-    //        //        let humidity: Service = Service(serviceIcon: UIImage(named: "WaterDrop")!, serviceName: "Humidity (%)")
-    //        //        let pressure: Service = Service(serviceIcon: UIImage(named: "Barometer")!, serviceName: "Pressure (Pa)")
-    //        //        let temperature: Service = Service(serviceIcon: UIImage(named: "Thermometer")!, serviceName: "Temperature (°F)")
-    //        //        let altitude: Service = Service(serviceIcon: UIImage(named: "Altitude")!, serviceName: "Altitude (m)")
-    //        //        
-    //        //        let allEyewearServices: [Service] = [batteryLevel, autoMode, manualMode]
-    //        //        let allFitnessServices: [Service] = [steps, calories, distance]
-    //        //        let allHealthServices: [Service] = [heartRate, oxygenLevel, skinTemperature]
-    //        //        let allEnvironmentServices: [Service] = [humidity, pressure, temperature, altitude]
-    //        //        
-    //        //        allServices[.Eyewear] = allEyewearServices
-    //        //        allServices[.Fitness] = allFitnessServices
-    //        //        allServices[.Health] = allHealthServices
-    //        //        allServices[.Environment] = allEnvironmentServices
-    //    }
-    
     func getAllServices(under serviceType: ServiceType)-> [Service]
     {
         switch serviceType
